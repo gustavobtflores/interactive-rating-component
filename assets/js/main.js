e2f02ccb874774a375c5871346bc72e887a6d8de
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-03-21 15:58:32 -0300

    Insert button to go back in navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const ratings = document.querySelectorAll(".rating-container_scale-item");
 const submitButton = document.querySelector(".rating-container_submit");
 const ratingContainer = document.querySelector(".rating-container");
 const thanksContainer = document.querySelector(".thanks-container");
+const goBackButton = document.querySelector(".thanks-container_go-back");
 var selectedRating = document.querySelector(".thanks-container_rating-selected");
 
 const removeSelected = () => {
@@ -32,4 +33,16 @@ submitButton.addEventListener("click", (event) => {
       thanksContainer.style.opacity = 1;
     }, 400);
   }
+});
+
+goBackButton.addEventListener("click", (event) => {
+  thanksContainer.style.opacity = 0;
+  setTimeout(() => {
+    thanksContainer.classList.add("hidden");
+    ratingContainer.classList.remove("hidden");
+  }, 200);
+  setTimeout(() => {
+    ratingContainer.style.opacity = 1;
+    submitButton.style.cursor = "pointer";
+  }, 400);
 });
